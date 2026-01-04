120b563d6615ca961f34a94ad5655a3bac5be2ec
Anoop Kumar A <92391766+AnoopkumarA@users.noreply.github.com>
2026-01-04 18:27:01 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 pt-32">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
+              <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl">
                 <span className="block">Transform Your Car's</span>
                 <span className="block text-indigo-500">Colors in Real-Time</span>
               </h1>
